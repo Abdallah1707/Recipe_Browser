4406c7d7b1699cf3dddc7ac30ab7d4888ccbede7
Ahmed Bahaa <s23511535eg@std.aou.edu.eg>
2026-08-05 19:19:40 +0300

    add animation to splash and edit splashActivity

--- app/src/main/java/com/example/recipe_browser/activities/SplashActivity.kt
+++ app/src/main/java/com/example/recipe_browser/activities/SplashActivity.kt
@@ -1,32 +1,68 @@
 package com.example.recipe_browser.activities
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.recipe_browser.R
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val logo = findViewById<ImageView>(R.id.imgLogo)
+        val title = findViewById<TextView>(R.id.txtTitle)
+        val sub = findViewById<TextView>(R.id.txtSub)
+
+        val dot1 = findViewById<View>(R.id.dot1)
+        val dot2 = findViewById<View>(R.id.dot2)
+        val dot3 = findViewById<View>(R.id.dot3)
+
+        // Logo Animation
+        logo.startAnimation(
+            AnimationUtils.loadAnimation(this, R.anim.logo_anim)
+        )
+
+        // Text Animation
+//        title.startAnimation(
+//            AnimationUtils.loadAnimation(this, R.anim.text_anim)
+//        )
+//
+//        sub.startAnimation(
+//            AnimationUtils.loadAnimation(this, R.anim.text_anim)
+//        )
+
+        // Dots Animation
+        val dotsAnim = AnimationUtils.loadAnimation(this, R.anim.dots_anim)
+
+        dot1.startAnimation(dotsAnim)
+
         Handler(Looper.getMainLooper()).postDelayed({
-            checkLoginStatus()
-        }, 3000)
-    }
+            dot2.startAnimation(
+                AnimationUtils.loadAnimation(this, R.anim.dots_anim)
+            )
+        }, 200)
 
-    private fun checkLoginStatus() {
-        val sharedPref = getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
-        val isLoggedIn = sharedPref.getBoolean("isLoggedIn", false)
+        Handler(Looper.getMainLooper()).postDelayed({
+            dot3.startAnimation(
+                AnimationUtils.loadAnimation(this, R.anim.dots_anim)
+            )
+        }, 400)
 
-        if (isLoggedIn) {
-            startActivity(Intent(this, MainActivity::class.java))
-        } else {
+        // Go To Login
+        Handler(Looper.getMainLooper()).postDelayed({
+
             startActivity(Intent(this, LoginActivity::class.java))
-        }
-        finish()
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+
+        }, 3000)
     }
 }
