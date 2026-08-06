489244f2c072ad0b0c9e2662c3a408f754d4d482
Abdallah1707 <abdallahmomen1707@gmail.com>
2026-08-06 15:16:50 +0300

    Revert "Implement Room database and DAOs"

--- app/src/main/java/com/example/recipe_browser/activities/MainActivity.kt
+++ app/src/main/java/com/example/recipe_browser/activities/MainActivity.kt
@@ -1,32 +1,20 @@
 package com.example.recipe_browser.activities
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import com.example.recipe_browser.R
-import com.example.recipe_browser.model.local.database.RecipeDatabase
-import com.example.recipe_browser.model.local.entities.RecipeEntity
-import kotlinx.coroutines.launch
+import com.example.recipe_browser.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val welcomeText = findViewById<TextView>(R.id.welcome_text)
-        val btnLogout = findViewById<Button>(R.id.btn_logout)
 
-        val sharedPref = getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
-        val userEmail = sharedPref.getString("userEmail", "User")
-        
-        welcomeText.text = "Welcome, $userEmail!"
-        btnLogout.setOnClickListener {
-            sharedPref.edit().putBoolean("isLoggedIn", false).apply()
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(android.R.id.content, HomeFragment())
+                .commit()
         }
     }
 }
