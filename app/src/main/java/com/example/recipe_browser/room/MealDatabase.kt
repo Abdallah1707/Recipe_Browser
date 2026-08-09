0ecb48de7f30f57a20ac04008dd15de89d1208fb
Ahmed Bahaa <s23511535eg@std.aou.edu.eg>
2026-08-09 06:09:31 +0300

    fix error data

--- app/src/main/java/com/example/recipe_browser/room/MealDatabase.kt
+++ app/src/main/java/com/example/recipe_browser/room/MealDatabase.kt
@@ -5,24 +5,34 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [User::class], version = 2)
+@Database(
+    entities = [User::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class MealDatabase : RoomDatabase() {
+
     abstract fun userDao(): UserDao
 
     companion object {
+
         @Volatile
         private var INSTANCE: MealDatabase? = null
 
         fun getDatabase(context: Context): MealDatabase {
+
             return INSTANCE ?: synchronized(this) {
+
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     MealDatabase::class.java,
-                    "recipe_database"
+                    "user_database"
                 )
                     .fallbackToDestructiveMigration()
                     .build()
+
                 INSTANCE = instance
+
                 instance
             }
         }
